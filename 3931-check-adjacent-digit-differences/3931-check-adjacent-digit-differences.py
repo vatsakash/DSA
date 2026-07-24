70dc113971928cedf283c94cca34597ee631f737
Akash Vats <vats9353@gmail.com>
2026-07-24 23:53:41 +0530

    Time: 3 ms (92.17%), Space: 12.3 MB (89.4%) - LeetHub

---
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAdjacentDiffAtMostTwo(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        dif = []
+        for i in range(1,len(s)):
+            first_dig = ord(s[i-1]) - ord('0')
+            adj_dig = ord(s[i]) - ord('0')
+            dif = abs(adj_dig - first_dig)
+            if dif > 2:
+                break
+            if i == len(s)-1:
+                if dif <= 2:
+                    return True
+        return False
